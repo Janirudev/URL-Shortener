977c8ec069a526ed9367b4acda26e4fbfc6f91a4
Janiru Vishmith <me@janiru.dev>
2022-08-05 18:13:16 +0400

    Spritzee homepage and /re/{id} redirection

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const Url = require('../models/Url');
 
+router.get('/', (req, res) => {
+  res.json({ msg: 'Welcome to Spritzee Server 🎂, A link shortener project.' });
+});
+
 router.get('/:shortUrl', async (req, res) => {
   const { shortUrl } = req.params;
 
@@ -9,6 +13,22 @@ router.get('/:shortUrl', async (req, res) => {
     let url = await Url.findOne({ shortUrl });
     if (url) {
       res.json(url);
+    } else {
+      res.status(404).json({ msg: 'No url found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: 'Internal server error' });
+  }
+});
+
+router.get('/r/:shortUrl', async (req, res) => {
+  const { shortUrl } = req.params;
+
+  try {
+    let url = await Url.findOne({ shortUrl });
+    if (url) {
+      res.redirect(url.longUrl);
     } else {
       res.status(404).json({ msg: 'No url found' });
     }
